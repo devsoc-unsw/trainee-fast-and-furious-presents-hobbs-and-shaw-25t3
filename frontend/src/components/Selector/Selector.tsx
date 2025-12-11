e9f36cf22b51ee67a02a3f9a2930779e373907c1
Nicole <z5677783@ad.unsw.edu.au>
2025-12-11 16:25:55 +1100

    Fix selector display

--- frontend/src/components/Selector/Selector.tsx
+++ frontend/src/components/Selector/Selector.tsx
@@ -4,12 +4,13 @@ type SelectorProps = {
     emojis: Map<string, () => void>;
 }
 
-const Selector = (props: SelectorProps) => {
+const Selector = (data: SelectorProps) => {
+    const emojiEntries = data.emojis.entries();
     return (
         <>
             <div id={styles.selector}>
                 {
-                    Object.entries(props.emojis).map(([key, val]) =>
+                    emojiEntries.map(([key, val]) =>
                         <button className={styles.emojiButton} onClick={val}>{key}</button>
                     )
                 }
