d60a8ba9441325189ff8ef2b8c98c5573499b2a0
Nicole <z5677783@ad.unsw.edu.au>
2025-12-16 00:33:59 +1100

    Change selector to pass in a function onSelect as prop

--- frontend/src/components/Selector/Selector.tsx
+++ frontend/src/components/Selector/Selector.tsx
@@ -1,22 +1,26 @@
 import styles from './Selector.module.css';
 
 type SelectorProps = {
-    emojis: Map<string, () => void>;
+    emojis: Map<string, string>;
+    onSelect: (value: string) => void;
 }
 
-const Selector = (data: SelectorProps) => {
-    const emojiEntries = data.emojis.entries();
-    return (
-        <>
-            <div id={styles.selector}>
-                {
-                    emojiEntries.map(([key, val]) =>
-                        <button className={styles.emojiButton} onClick={val}>{key}</button>
-                    )
-                }
-            </div>
-        </>
-    );
-}
+const Selector = ({ emojis, onSelect }: SelectorProps) => {
+  return (
+    <div className={styles.selector}>
+      {Array.from(emojis.entries()).map(([emoji, value]) => (
+        <button
+          key={emoji}
+          className={styles.emojiButton}
+          onClick={() => onSelect(value)}
+          type="button"
+          aria-label={value}
+        >
+          {emoji}
+        </button>
+      ))}
+    </div>
+  );
+};
 
 export { Selector };
