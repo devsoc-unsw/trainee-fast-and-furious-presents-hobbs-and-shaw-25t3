2e6ccf4a1c799bd786c2c9112d0d7c7523c5823e
Jared Lucas Amistad Schulz <gorillacyborgz@gmail.com>
2025-12-18 14:47:51 +1100

    initial setup for vincent

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,26 +4,28 @@ import type { SelectorPageProps } from './utils/types'
 import './App.css'
 import { Routes, Route } from "react-router-dom";
 
-const dummyData: SelectorPageProps = {
-  question: "My Question",
-  field: "mood",
-  emojis: new Map([
-          ['🥳', 'excited'],
-          ['🥰', 'loved'],
-          ['😭', 'sad'],
-          ['🙂', 'okay'],
-          ['😴', 'tired'],
-          ['😡', 'angry'],
-          ]),
-  to: '/next'
-}
+const dummyData: SelectorPageProps[] = [
+  {
+    question: "What are you feeling right now?",
+    field: "mood",
+    emojis: new Map([
+            ['🥳', 'excited'],
+            ['🥰', 'loved'],
+            ['😭', 'sad'],
+            ['🙂', 'okay'],
+            ['😴', 'tired'],
+            ['😡', 'angry'],
+            ]),
+    to: '/next'
+  },
+]
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<LandingPage/>}/>
-        {/* <Route path='/' element={<SelectorPage question={dummyData.question} field={dummyData.field} emojis={dummyData.emojis} to={dummyData.to} />}/> */}
+        {/* <Route path='/' element={<LandingPage/>}/> */}
+      <Route path='/' element={<SelectorPage question={dummyData[0].question} field={dummyData[0].field} emojis={dummyData[0].emojis} to={dummyData[0].to} />}/>
       </Routes>
     </>
   );
