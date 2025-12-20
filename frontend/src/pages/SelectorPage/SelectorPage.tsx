6c1a97fb813ce140f31252e90d48310bd95c9b4b
Nicole <z5677783@ad.unsw.edu.au>
2025-12-20 18:09:21 +1100

    Add distance slider to selector page

--- frontend/src/pages/SelectorPage/SelectorPage.tsx
+++ frontend/src/pages/SelectorPage/SelectorPage.tsx
@@ -4,24 +4,32 @@ import type { SelectorPageProps } from "../../utils/types";
 import styles from "./SelectorPage.module.css";
 import { useNavigate } from "react-router-dom";
 import { usePreferences } from "../../context/PreferenceContext";
+import { Slider } from "../../components/Slider/Slider";
+import { useState } from "react";
 
 const SelectorPage = (props: SelectorPageProps) => {
   const navigate = useNavigate();
   const { updatePreferences } = usePreferences();
-  const handleEmojiSelected = (value: string) => {
-      updatePreferences(props.field, value);
+  const isDistance = props.field === 'distance';
+
+  const handleSelected = (value: string | number) => {
+      updatePreferences(props.field, String(value));
       navigate(props.to);
   };
+
+  const [distance, setDistance] = useState<number>(5);
 
   return (
     <div className={styles.page}>
       <h1 className={styles.heading}>{props.question}</h1>
 
-      <Selector emojis={props.emojis} onSelect={handleEmojiSelected} />
+      {isDistance ? <Slider value={distance} onChange={setDistance}/>
+      : <Selector emojis={props.emojis} onSelect={handleSelected} />}
 
       <div className={styles.navButtons}>
         <Button text="Back" onClick={() => navigate(-1)} />
-        <Button text="Let Us Decide!" onClick={() => navigate(props.to)} />
+        {isDistance ? <Button text="Continue" onClick={() => handleSelected(distance)} />
+        : <Button text="Let Us Decide!" onClick={() => navigate(props.to)} />}
       </div>
     </div>
   );
